ef16339ab9f49f9e2b506a8c8e44ef5212d979e6
Yasoob <yasoob.khld@gmail.com>
2020-06-06 02:25:06 -0400

    updated the script to work :sparkle:

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
-### Wordpress to Staticman Comments
+## Wordpress to Staticman Comments
 
-This is a rough script to convert Wordpress comments to staticman comments
+The original script was written by [Dan](https://github.com/dancwilliams/wordpress_to_staticman_comments) as a quick-fix for his usage. I don't know if he intended for anyone else to use it eventually. I ended up modifying his script quite a lot to suit my own needs while moving from Wordpress to Hugo. Needless to say, the script is still rough and I modified it just enough so that my own use-cases were being catered to. I am hopeful that it will work fine for you too but try it at your own risk. Or maybe you can fork this and add the functionality you want and open up a PR? :smile:
 
-This script uses the XML export from Wordpress to convert comments to YAML format.
+### Requirements
 
-I admit that this is very rough, but it helped me get the job done.
+- Python 3
+- `xmltodict` (`pip install xmltodict`)
 
-I just wanted to capture the logic.
+### Run
+
+- Rename your wordpress xml dump file to `big_data.xml` and put it in the same folder as this script. 
+- Create a `comments` folder in the same folder as this script.
+- Run `python wordpress_to_staticman.py`
+
+In the end just copy the `comments` folder to the `data` folder in the base directory of your Hugo source files. 
+
+### Nested replies
+
+If you don't already have nested comments functionality working in your Staticman installation, you can follow [this](https://yasoob.me/posts/running_staticman_on_static_hugo_blog_with_nested_comments/) tutorial to set that up. The output of this script will also work with nested replies!
--- wordpress_to_staticman.py
+++ wordpress_to_staticman.py
@@ -1,59 +1,78 @@
 import xmltodict
 import time
 import hashlib
+import sys
+import os
+import traceback
+from pprint import pprint
+import yaml
+
+if not os.path.exists(os.path.join(os.getcwd(), 'big_data.xml')):
+    sys.exit("Please put the big_data.xml file in this folder")
 
 total_count = 0
-m = hashlib.md5()
-
 with open('big_data.xml') as fd:
     cfg = xmltodict.parse(fd.read())
 
 for item in cfg['rss']['channel']['item']:
-    print ""
-    print "Title: " + item['title']
-    print ""
+    foldername = '-'.join(item['link'].split('.com/')[1].split('/'))[:-1]
     try:
+        if item.get('wp:comment') == None:
+            continue
         for comment in item['wp:comment']:
+            if comment['wp:comment_approved'] != '1' or comment['wp:comment_type'] == 'pingback':
+                continue
             total_count += 1
+            data_dict = {}
+            folderpath = os.path.join(os.getcwd(), 'comments', foldername)
+            total_count += 1
+            if not os.path.exists(folderpath):
+                try:
+                    os.mkdir(folderpath)
+                except Exception as e:
+                    print("Error occured")
+                    sys.exit()
             try:
                 date_time = comment['wp:comment_date_gmt']
                 pattern = '%Y-%m-%d %H:%M:%S'
                 epoch = int(time.mktime(time.strptime(date_time, pattern)))
-                print "Comment File Name: comment-" + str(epoch) + ".yml"
+                comment_file = "comment-" + str(epoch) + ".yml"
+            except:
+                continue
+            try:
+                data_dict['_id'] = comment['wp:comment_id']
             except:
                 pass
             try:
-                print "_id: " + comment['wp:comment_id']
-            except:
-                pass
-            try:
-                print "name: " + comment['wp:comment_author']
+                data_dict['name'] = comment['wp:comment_author']
             except:
                 pass
             try:
                 if comment['wp:comment_parent'] != '0':
-                    print "reply_to: " + comment['wp:comment_parent']
+                    data_dict['reply_to'] = comment['wp:comment_parent']
             except:
                 pass
             try:
                 temp_email = comment['wp:comment_author_email']
                 temp_email = temp_email.strip()
                 temp_email = temp_email.lower()
-                m.update(temp_email)
+                data_dict['email'] =  hashlib.md5(temp_email).hexdigest()
+            except:
+                pass
+            try:
+                data_dict['comment'] = comment['wp:comment_content']
+            except:
+                pass
+            try:
+                data_dict['date'] = comment['wp:comment_date_gmt']
+            except:
+                pass
+            pprint(data_dict)
+            with open(os.path.join(folderpath, comment_file), "w") as f:
+                yaml.dump(data_dict, f)
 
-                print "email: " + m.hexdigest()
-            except:
-                pass
-            try:
-                print "body: " + comment['wp:comment_content']
-            except:
-                pass
-            try:
-                print "date: " + comment['wp:comment_date_gmt']
-            except:
-                pass
-            print ""
-    except:
+    except Exception as e:
+        traceback.print_exc() 
         pass
 
-print "Total Count: " + str(total_count)
+print("Total Count: " + str(total_count))
